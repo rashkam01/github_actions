b32bbfbb9c1e173791cb32cdafe8891022815939
Rashmi <rashmi.kamath01@gmail.com>
2024-10-30 15:00:00 +0100

    test file refactored again

--- file_test.py
+++ file_test.py
@@ -1,12 +1,12 @@
-
-# This is a substraction function 
-def test_calc_substraction():
-    #Function test the output of 2-4
+# This is a subtraction function
+def test_calc_subtraction():
+    # Function tests the output of 2 - 4
     output = 2 - 4
     assert output == -2
 
-# This is multiply function
+
+# This is a multiplication function
 def test_calc_multiply():
-    # Function test the output of 2*4
+    # Function tests the output of 2 * 4
     output = 2 * 4
     assert output == 8
